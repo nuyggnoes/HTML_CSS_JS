bcd35503319f699f252af09e17cd912f330ee2ff
임성균 <97024187+nuyggnoes@users.noreply.github.com>
2024-05-15 16:31:40 +0900

    todo [addToDo]

--- to-do/main.js
+++ to-do/main.js
@@ -1,12 +1,11 @@
-document.querySelector(".add-todo").addEventListener("click", function () {
-  var input = document.querySelector("todo-input");
-  var todoText = input.value.trim();
-  console.log(input);
-  console.log(todoText);
-  if (todoText !== "") {
-    var li = document.createElement("li");
-    li.textContent = todoText;
-    document.querySelector(".todo-list").appendChild(li);
-    input.value = "";
-  }
-});
+const toDoForm = document.querySelector("#todo-form");
+const toDoInput = document.querySelector("#todo-form input");
+
+function addToDo(event) {
+  event.preventDefault();
+  const newToDo = toDoInput.value;
+  console.log(newToDo);
+  toDoInput.value = "";
+}
+
+toDoForm.addEventListener("submit", addToDo);
