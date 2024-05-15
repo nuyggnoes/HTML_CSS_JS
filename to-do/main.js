09f2accecb30b5016094f535b1f0b4cec00e48ab
임성균 <97024187+nuyggnoes@users.noreply.github.com>
2024-05-15 16:42:05 +0900

    todo [deleteToDo]

--- to-do/main.js
+++ to-do/main.js
@@ -1,11 +1,36 @@
 const toDoForm = document.querySelector("#todo-form");
 const toDoInput = document.querySelector("#todo-form input");
+const toDoList = document.querySelector("#todo-list");
 
 function addToDo(event) {
   event.preventDefault();
   const newToDo = toDoInput.value;
-  console.log(newToDo);
   toDoInput.value = "";
+  const newToDoObj = {
+    text: newToDo,
+    id: Date.now(),
+  };
+  // local storage에 저장해서 새로고침해도 데이터가 안 날아가게 처리(나중에)
+  // 화면에 뿌리기
+  paintToDo(newToDoObj);
+}
+
+function paintToDo(newToDo) {
+  const li = document.createElement("li");
+  li.id = newToDo.id;
+  const span = document.createElement("span");
+  span.innerText = newToDo.text;
+  const button = document.createElement("button");
+  button.innerText = "❌";
+  button.addEventListener("click", deleteToDo);
+  li.appendChild(span);
+  li.appendChild(button);
+  toDoList.appendChild(li);
+}
+
+function deleteToDo(event) {
+  const li = event.target.parentElement;
+  li.remove();
 }
 
 toDoForm.addEventListener("submit", addToDo);
